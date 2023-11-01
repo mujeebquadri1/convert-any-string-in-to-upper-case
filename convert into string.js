7be35e42cc025d812c14990d1078d877c5abd145
mujeebquadri1 <149534513+mujeebquadri1@users.noreply.github.com>
2023-11-01 20:02:15 +0530

    Add files via upload

---
+++ convert into string.js
@@ -0,0 +1,15 @@
+//convert string into uppercase
+//program to add two numbers with dom
+//with validations
+
+function upper_case() {  
+var numberFisrt=document.getElementById("number1").value;   //input  values with validation
+
+ var data= numberFisrt;
+ 
+ 
+document.getElementById('mylocation').innerHTML=data.toUpperCase();  
+document.getElementById("mylocation").style.color = "green"; 
+
+ 
+ }  
